1294d4d41ab4f7163e6c160e1cd3929b934db5c3
ngalantowicz <ngalantowicz@gmail.com>
2016-12-19 14:38:44 -0500

    setup map.js file to render openlayer map, setup sass files to style first map, setup html template state to use for angular and map html

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+
+(function() {
+    'use strict';
+
+    var view = new ol.View({
+        center: [ -6217890.205764902, -1910870.6048274133 ],
+        zoom: 4,
+        maxZoom: 18,
+        minZoom: 2
+    });
+
+    var baseLayer = new ol.layer.Tile({
+        source: new ol.source.OSM({ layer: 'osm' })
+    });
+
+    map = new ol.Map({
+        target: 'map',
+        controls: ol.control.defaults().extend([
+            new ol.control.ScaleLine(),
+            new ol.control.ZoomSlider()
+        ]),
+        renderer: 'canvas',
+        layers: [baseLayer],
+        view: view
+    });
+}());
