0a7fba2dfad31ea6785ee751b7c4c9e1ca7fe90b
ngalantowicz <ngalantowicz@gmail.com>
2016-12-19 14:35:46 -0500

    added openlayers,grunt-contrib-sass, and jquery dependency in package json, configed gruntfile to concat necesarry js files from said dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -5,7 +5,7 @@
 
         grunt.initConfig({
 
-            clean: ['public/js/', 'public/index.html', 'public/styles.css'],
+            clean: ['public/js/', 'public/index.html', 'public/styles.css', 'public/styles.css.map'],
 
             jshint: {
                 options: {
@@ -44,15 +44,54 @@
 
             concat: {
                 js: {
-                    src: [],
-                    dest: ''
+                    src: [ 'app/assets/javascripts/**.*js'],
+                    dest: 'public/js/app.js'
                 },
+                vendorjs: {
+                    files: [
+                        {
+                            expand: true,
+                            cwd: 'node_modules/jquery/dist/',
+                            src: [ 'jquery.js' ],
+                            dest: 'public/js'
+
+                        },
+                        {
+                            expand: true,
+                            cwd: 'node_modules/openlayers/dist',
+                            src: [ 'ol.js' ],
+                            dest: 'public/js'
+                        },
+                        {
+                            expand: true,
+                            cwd: 'node_modules/openlayers/dist',
+                            src: 'ol.css',
+                            dest: 'public/'
+                        }
+                    ]
+                }
+            },
+
+            sass: {
+                allStyles: {
+                    files: {
+                        'public/styles.css': 'app/assets/stylesheets/sass/main.scss'
+                    }
+                }
             },
 
             watch: {
                 html: {
                     files: ['app/assets/templates/index.html'],
                     tasks: ['copy:html']
+                },
+                sass: {
+                    files: ['app/assets/stylesheets/sass/**/*.scss'],
+                    tasks: ['sass']
+                },
+                js: {
+                    files: ['app/assets/javascripts/**/*.js'],
+                    tasks: ['test', 'concat']
                 }
             }
         });
@@ -62,7 +101,8 @@
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-contrib-concat');
         grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-contrib-sass');
 
         grunt.registerTask('test', ['jshint'] );
-        grunt.registerTask('default', [ 'test', 'clean', 'copy' ] );
+        grunt.registerTask('default', [ 'test', 'clean', 'copy', 'concat' ] );
     };
