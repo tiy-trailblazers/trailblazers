e0b89dc1537b7e27752cf98101ed8199b55cbcf7
ngalantowicz <ngalantowicz@gmail.com>
2016-12-20 08:59:13 -0500

    Set up event handler for map click that appends marker with location data

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,25 +1,49 @@
 (function() {
     'use strict';
 
-    var view = new ol.View({
-        center: [ -6217890.205764902, -1910870.6048274133 ],
-        zoom: 4,
-        maxZoom: 18,
-        minZoom: 2
-    });
+
 
     var baseLayer = new ol.layer.Tile({
-        source: new ol.source.OSM({ layer: 'osm' })
+        source: new ol.source.XYZ({
+            url: 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmdhbGFudG93aWN6IiwiYSI6ImNpd3dsNmhyZjAxNWUydHFyNnhjbzZwc3QifQ._xkfHwZJ1FsueAu0K6oQeg' })
     });
 
-    map = new ol.Map({
+    var map = new ol.Map({
         target: 'map',
         controls: ol.control.defaults().extend([
-            new ol.control.ScaleLine(),
             new ol.control.ZoomSlider()
         ]),
         renderer: 'canvas',
         layers: [baseLayer],
-        view: view
+        view: new ol.View({
+            center: [ -10853463.910959221, 4789639.227729736 ],
+            zoom: 4,
+            maxZoom: 18,
+            minZoom: 2
+        })
     });
+
+    var popup = new ol.Overlay({
+        element: document.getElementById('popup')
+      });
+      map.addOverlay(popup);
+
+      map.on('click', function(evt) {
+          console.log(evt);
+        var element = popup.getElement();
+        var coordinate = evt.coordinate;
+        var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
+            coordinate, 'EPSG:3857', 'EPSG:4326'));
+        console.log(hdms);
+
+        $(element).popover('destroy');
+        popup.setPosition(coordinate);
+        $(element).popover({
+          'placement': 'top',
+          'animation': false,
+          'html': true,
+          'content': '<p>The location you clicked was:</p><code>' + hdms + '</code>'
+        });
+        $(element).popover('show');
+      });
 }());
