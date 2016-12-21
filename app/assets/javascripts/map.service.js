3401a6f6dd6fc2413699d12526967329955e4dac
ngalantowicz <ngalantowicz@gmail.com>
2016-12-21 18:00:57 -0500

    wrote doc blocks for angular service MapService

--- app/assets/javascripts/map.service.js
+++ app/assets/javascripts/map.service.js
@@ -6,12 +6,21 @@
 
     MapService.$inject = [ '$http' ];
 
+    /**
+     * Constructs angular service for trail and campground http requests
+     * @param {Service} $http core angular service for http requests
+     */
     function MapService($http){
 
         return {
             findTrails: findTrails
         };
 
+        /**
+         * executes http request to app backend for trail and campground data
+         * @param  {Array} coordinates location data based off radius rectangle
+         * @return {Promise} angular promise functions            [description]
+         */
         function findTrails(coordinates){
             console.log(coordinates);
             var west = coordinates[0];
