825343576c0bf66fdf59f4753d391970eb352741
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-12-30 22:57:41 -0600

    Created create route for user and tested. Created update route, didnt test yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :trips
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
+  has_secure_password
 end
