04b0c08d0a06f89ac4cfa7fe9fbf092bf6a07c3b
Aileen Rowan <aileen.s.rowan@gmail.com>
2017-01-06 09:28:42 -0500

    Added field for profile image to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105182524) do
+ActiveRecord::Schema.define(version: 20170106142709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20170105182524) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
+    t.text     "profile_image"
   end
 
 end
