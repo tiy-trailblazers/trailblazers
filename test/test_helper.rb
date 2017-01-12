e11ba0190fecced3b3e582d1b44a458fafc12257
Aileen Rowan <aileen.s.rowan@gmail.com>
2017-01-12 12:04:11 -0500

    Began setup of capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,23 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'webmock/minitest'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include Capybara::DSL
 
-  # Add more helper methods to be used by all tests here...
+  # Reset sessions and driver between tests
+  # Use super wherever this method is redefined in your individual test classes
+
+  Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
+  Capybara.javascript_driver = :selenium_chrome
+
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
 end
